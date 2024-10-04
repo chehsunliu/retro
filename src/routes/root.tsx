5310238408962718d2cf688413fcbdb4f52dba25
Che-Hsun Liu <chehsunliu@gmail.com>
2024-10-05 00:45:49 +0800

    Add basic top bar

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,25 +1,50 @@
-import { Outlet, Link } from "react-router-dom";
+import React from "react";
+import { Outlet, useNavigate, Link } from "react-router-dom";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 function Root() {
+  const navigate = useNavigate();
+  const [selection, setSelection] = React.useState("");
+
+  const handleSelectionChange = (value: string) => {
+    setSelection(value);
+    navigate(value);
+  };
+
+  const handleHomeClick = () => {
+    setSelection("");
+  };
+
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to={""}>Home</Link>
-          </li>
-          <li>
-            <Link to={"pal"}>PAL</Link>
-          </li>
-          <li>
-            <Link to={"swd-2e"}>SWD2E</Link>
-          </li>
-        </ul>
-      </nav>
-      <div>
+    <div>
+      <header className={"flex h-16 border-b px-36"}>
+        <nav className={"flex flex-row grow gap-6 items-center"}>
+          <Link onClick={handleHomeClick} to={"/"}>
+            Retro
+          </Link>
+          <div className={"grow"} />
+          <Select onValueChange={handleSelectionChange} value={selection}>
+            <SelectTrigger className={"w-[180px]"}>
+              <SelectValue placeholder="--" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="/swd-2e">SWD2E</SelectItem>
+              <SelectItem value="/pal">PAL</SelectItem>
+            </SelectContent>
+          </Select>
+        </nav>
+      </header>
+      <main>
         <Outlet />
-      </div>
-    </>
+      </main>
+    </div>
   );
 }
 
