731b6e760ec70a224d5d7992ced79da7e7f4f97c
Che-Hsun Liu <chehsunliu@gmail.com>
2024-10-11 15:35:01 +0800

    Update message

--- src/lib/tracer.ts
+++ src/lib/tracer.ts
@@ -1,12 +1,10 @@
 function createTracer() {
   if (import.meta.env.DEV) {
-    console.log("Disable tracer");
     return {
       gtag: () => {},
     };
   }
 
-  console.log("Enable tracer");
   return { gtag: window.gtag };
 }
 
