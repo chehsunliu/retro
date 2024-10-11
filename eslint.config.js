e191124adef8c2c6fee1313ee6b622c5a8b26980
Che-Hsun Liu <chehsunliu@gmail.com>
2024-10-11 11:14:40 +0800

    Fix lint error

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", "src/components/ui"] },
+  { ignores: ["dist"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
